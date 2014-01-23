5e53f66180c70b2115f3b3e067d50014df647e8e
DanielHiggs <daniel.higgs@gmail.com>
2014-01-23 12:09:45 -0500

    Updated u/i

--- TestSuite.m
+++ TestSuite.m
@@ -7,21 +7,39 @@ while true
 	bar = foo.select();
 	bar.run();
 	
-	yesno = input('\n\nSave This Data? [Y/N]: ', 's');
-	if upper(yesno) == 'Y'
-		while true
-			prefix = input('\nEnter filename [*.mat]: ', 's');
-			[status, files] = bar.save(prefix);
+	commands = bar.get_commands();
+	
+	if isfield(commands, 'name') & any(strcmp({commands.name}, 'import'))
+		yesno = input('\n\nImport This Data Into Current Workspace? [Y/N]: ', 's');
+		if upper(yesno) == 'Y'
+			[status, variables] = bar.import();
 			if status ~= 1
-				fprintf('Saving data to ./%s/\n\n', prefix);
-				for i=1:numel(files)
-					fprintf('%s\n', files{i});
+				for variable=variables
+					fprintf('Saving %s to variable ''%s''\n', variable.description, variable.name);
 				end
 				fprintf('Done!\n\n');
-				break
-			else
-				fprintf('%s Directory Already Exists\n', prefix);
-				fprintf('Please enter a new directory\n');
+			end
+			break
+		end
+	end
+	
+	if isfield(commands, 'name') & any(strcmp({commands.name}, 'save'))
+		yesno = input('\n\nSave This Data? [Y/N]: ', 's');
+		if upper(yesno) == 'Y'
+			while true
+				prefix = input('\nEnter filename [*.mat]: ', 's');
+				[status, files] = bar.save(prefix);
+				if status ~= 1
+					fprintf('Saving data to ./%s/\n\n', prefix);
+					for i=1:numel(files)
+						fprintf('%s\n', files{i});
+					end
+					fprintf('Done!\n\n');
+					break
+				else
+					fprintf('%s Directory Already Exists\n', prefix);
+					fprintf('Please enter a new directory\n');
+				end
 			end
 		end
 	end
